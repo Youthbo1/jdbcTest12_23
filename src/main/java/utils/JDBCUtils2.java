7d273abbf1311c4df6cff8f0953d8c5b8adc15eb
bo <389616868@qq.com>
2018-01-19 15:17:13 +0800

    1-19-c3p0连接池

---
+++ src/main/java/utils/JDBCUtils2.java
@@ -0,0 +1,74 @@
+package utils;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * JDBC的工具类
+ *
+ */
+public class JDBCUtils2 {
+	private static final ComboPooledDataSource dataSource = new ComboPooledDataSource();
+	
+	/**
+	 * 获得连接的方法:
+	 * @throws SQLException 
+	 */
+	public static Connection getConnection() throws Exception{
+		Connection conn = dataSource.getConnection();
+		return conn;
+	}
+	
+	/**
+	 * 资源释放
+	 */
+	public static void release(Statement stmt,Connection conn){
+		if(stmt != null){
+			try {
+				stmt.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			stmt = null;
+		}
+		if(conn != null){
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			conn = null;
+		}
+	}
+	
+	public static void release(ResultSet rs,Statement stmt,Connection conn){
+		if(rs!= null){
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			rs = null;
+		}
+		if(stmt != null){
+			try {
+				stmt.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			stmt = null;
+		}
+		if(conn != null){
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			conn = null;
+		}
+	}
+}
